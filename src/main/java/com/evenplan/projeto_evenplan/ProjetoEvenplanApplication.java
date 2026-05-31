1890f53c92c7fadb0de7ff21f815ee2093402dad
LaurenAmici <lauren.amici@gmail.com>
2026-05-31 17:07:05 -0300

    Initial commit

---
+++ src/main/java/com/evenplan/projeto_evenplan/ProjetoEvenplanApplication.java
@@ -0,0 +1,13 @@
+package com.evenplan.projeto_evenplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEvenplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEvenplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evenplan/projeto_evenplan/ProjetoEvenplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenplan.projeto_evenplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEvenplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
